e2f60d0aea780d9b4a5d5ef9e0c27f0865fcb87f
Courtney Cook <courtneycook82@gmail.com>
2021-10-27 09:36:28 -0400

    updated code for selecting a random image

--- outfit.js
+++ outfit.js
@@ -1,19 +1,27 @@
 console.log("hello");
 
-
-let myArray = [
+myArray = [
     "Carhartt Shirt.jpg",
     "Carhartt Sweatshirt.jpg",
     "Carhartt Vest.jpg"
 ];
 
-document.getElementsByTagName('img').src = myArray[randomItem];
-
 function choose_random_item() {
 
-    let randomItem = Math.floor(Math.random() * myArray.length);
-    let image = myArray[randomItem;]
+    randomItem = Math.floor(Math.random() * myArray.length);
+
+    selected_image = myArray[randomItem];
+
+    document.getElementById('image').src = `./images/${selected_image}`;
 }
+
+// document.body.innerHTML = randomItem;
+
+
+//     let image = myArray[randomItem];
+//     document.getElementsById('img').src = myArray[randomItem];
+// }
+
 
 // selected_item = myArray[randomItem]
 // document.getElementsByTagName('img').src = myArray[randomItem];
