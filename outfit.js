512633aa3ad467ecb2f3ebf3e896abb83c1de52f
Courtney Cook <courtneycook82@gmail.com>
2022-02-01 18:51:17 -0500

    commiting changes

--- outfit.js
+++ outfit.js
@@ -15,12 +15,14 @@ function choose_random_item() {
     document.getElementById('image').src = `./images/${selected_image}`;
 }
 
+
 setTimeout(() => {
     console.log("the callback works")
 }, 2000);
 
-window.onload = function() {
-    document.querySelector("#callback").addEventListener("click", function() {
-        console.log("Button was clicked")
-    });
-}
+// window.onload = function() {
+//     document.querySelector("#callback").addEventListener("click", function() {
+//         document.getElementById('image').src = "https://i0.wp.com/www.cssscript.com/wp-content/uploads/2015/11/ispinner.jpg?fit=400%2C298&ssl=1";
+//         console.log("Button was clicked")
+//     });
+// }
